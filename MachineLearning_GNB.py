521881eb4abf83b883ef017c84457f725dda3904
Aimee Schwab-McCoy <39537992+aimeeschwab-mccoy@users.noreply.github.com>
2023-02-13 12:38:39 -0600

    Add files via upload

--- MachineLearning_GNB.py
+++ MachineLearning_GNB.py
@@ -27,38 +27,29 @@ penguins = sns.load_dataset("penguins").dropna()
 penguins['species_int'] = penguins['species'].replace(to_replace = ['Adelie','Chinstrap', 'Gentoo'],
                                                       value = [int(0), int(1), int(2)])
 
-col1, col2 = st.columns([2, 3])
+a,b = st.slider("Use the slider to adjust the prior probabilties",
+    min_value=0.0, max_value=1.0, value=(0.33,0.67), step=0.01)
+
+col01, col02, col03 = st.columns([1, 1, 1])
+
+with col01:
+
+    st.write("P(Adelie):", round(a, 2))
+
+with col02:
+
+    st.write("P(Chinstrap):", round(b-a, 2))
+
+with col03:
+
+    st.write("P(Gentoo):", round(1-b, 2))
+
+col1, col2 = st.columns([1,1])
 
 with col1:
 
-    a,b = st.slider("Use the slider to adjust the prior probabilties",
-        min_value=0.0, max_value=1.0, value=(0.333,0.667), step=0.001)
-
-    st.write("P(Adelie):", round(a, 3))
-    st.write("P(Chinstrap):", round(b-a, 3))
-    st.write("P(Gentoo):", round(1-b, 3))
-
     X = penguins[['bill_length_mm']]
     y = penguins[['species_int']]
-
-    showtext = st.checkbox(label="Show description?", value=False)
-
-    if showtext:
-
-        '''
-        Top: Predicted probabilities for Adelie (blue solid line), Chinstrap (orange dashed line), 
-        and Gentoo (green dotted line) penguins using Gaussian naive Bayes. Changing the prior probabilities 
-        adjusts the fitted probability curves. 
-        
-        Bottom: Decision boundary plot based on bill length (mm). Decision boundary cutoffs correspond to 
-        values of bill length where the predicted probabilities intersect. The species with the highest probability
-        curve at a given value of bill length is the predicted class. Classes with greater prior probability have larger 
-        regions in the decision boundary plot.
-        '''
-
-
-with col2: 
-
     # Initialize and fit Gaussian naive Bayes with priors
     NBModel = GaussianNB(priors = [a, b-a, 1-b])
     NBModel.fit(X, np.ravel(y))
@@ -70,7 +61,7 @@ with col2:
     probAdelie = NBModel.predict_proba(xrange.reshape(-1, 1))[:, 0]
     probChinstrap = NBModel.predict_proba(xrange.reshape(-1, 1))[:, 1]
     probGentoo = NBModel.predict_proba(xrange.reshape(-1, 1))[:, 2]
- 
+
     plt.plot(xrange, probAdelie, color='#1f77b4', linewidth=2, linestyle='-')
     plt.plot(xrange, probChinstrap, color='#ff7f0e', linewidth=2, linestyle='--')
     plt.plot(xrange, probGentoo, color='#3ca02c', linewidth=2, linestyle=':')
@@ -78,6 +69,20 @@ with col2:
     plt.ylabel('Probability of each species', fontsize=14)
 
     st.pyplot(fig)
+
+    showtext = st.checkbox(label="Show probability curve description?", value=False)
+
+    if showtext:
+
+        '''
+        Left: Predicted probabilities for Adelie (blue solid line), Chinstrap (orange dashed line), 
+        and Gentoo (green dotted line) penguins using Gaussian naive Bayes. Changing the prior probabilities 
+        adjusts the fitted probability curves. 
+        '''
+
+
+with col2: 
+
 
     fig, ax = plt.subplots()
     contourf_kwargs = {'alpha': 0.2}
@@ -92,3 +97,14 @@ with col2:
     plt.ylabel('\n     ', fontsize=14)
 
     st.pyplot(fig)
+
+    showtext2 = st.checkbox(label="Show decision boundary description?", value=False)
+
+    if showtext2:
+
+        '''
+        Right: Decision boundary plot based on bill length (mm). Decision boundary cutoffs correspond to 
+        values of bill length where the predicted probabilities intersect. The species with the highest probability
+        curve at a given value of bill length is the predicted class. Classes with greater prior probability have larger 
+        regions in the decision boundary plot.
+        '''
